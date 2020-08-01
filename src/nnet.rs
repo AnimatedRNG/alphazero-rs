85faacf26a3ea0a7b9e45e242e0014ffe8f3733a
Srinivas Kaza <srinivas@kaza.io>
2020-07-31 20:54:45 -0400

    Fix BatchedValue type from View to owned type

--- src/nnet.rs
+++ src/nnet.rs
@@ -1,8 +1,10 @@
-use ndarray::{Array, ArrayD, ArrayView, ArrayViewD, Ix1, Ix2};
+use ndarray::{ArcArray, Array, ArrayD, ArrayView, ArrayViewD, Ix1, Ix2, IxDyn};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
 
 use crate::game::F;
+
+pub type ArcArrayD<I> = ArcArray<I, IxDyn>;
 
 pub type BoardFeatures = ArrayD<F>;
 pub type BoardFeaturesView<'a> = ArrayViewD<'a, F>;
@@ -14,7 +16,7 @@ pub type BatchedPolicyView<'a> = ArrayView<'a, f32, Ix2>;
 pub type Policy = Array<f32, Ix1>;
 pub type PolicyView<'a> = ArrayView<'a, f32, Ix1>;
 pub type SerializedPolicy = Vec<f32>;
-pub type BatchedValue<'a> = ArrayView<'a, f32, Ix1>;
+pub type BatchedValue = Array<f32, Ix1>;
 pub type Value = f32;
 
 #[derive(Serialize, Deserialize)]
@@ -24,10 +26,21 @@ pub struct TrainingSample {
     pub v: Value,
 }
 
+pub type ArcBatchedBoardFeatures = ArcArrayD<F>;
+pub type ArcBatchedPolicy = ArcArray<f32, Ix2>;
+pub type ArcBatchedValue = ArcArray<f32, Ix1>;
+
+pub type SOATrainingSamples<'a> = (
+    ArcBatchedBoardFeatures,
+    ArcBatchedPolicy,
+    ArcBatchedValue,
+);
+
 pub trait NNet: Send + Clone + 'static {
     fn new() -> Self;
 
-    fn train(&self, examples: Vec<TrainingSample>);
+    //fn train(&self, examples: Vec<TrainingSample>);
+    fn train(&self, examples: SOATrainingSamples);
 
     fn predict(&self, board: BatchedBoardFeaturesView) -> (BatchedPolicy, BatchedValue);
 
