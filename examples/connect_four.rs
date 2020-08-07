3f068da1c31a21c7a221dd9e63a8593d66e3112b
Srinivas Kaza <srinivas@kaza.io>
2020-08-06 21:57:14 -0400

    Random MCTS tree

--- examples/connect_four.rs
+++ examples/connect_four.rs
@@ -1,5 +1,8 @@
+use alphazero_rs::coach::Coach;
 use alphazero_rs::nnet::NNet;
 use ndarray::Array;
+use rand::rngs::SmallRng;
+use rand::SeedableRng;
 
 mod connect_four_lib;
 mod utils;
@@ -28,8 +31,11 @@ impl NNet for DumbConnectFourNnet {
         alphazero_rs::nnet::BatchedPolicy,
         alphazero_rs::nnet::BatchedValue,
     ) {
-        if let [batches, width, _height] = *board.shape() {
-            (Array::ones((batches, width)), Array::ones((batches,)))
+        if let [batches, _height, width] = *board.shape() {
+            (
+                Array::ones((batches, width)) / width as f32,
+                Array::ones((batches,)),
+            )
         } else {
             panic!()
         }
@@ -46,5 +52,29 @@ fn ask_for_action(game: &ConnectFourGame) -> u8 {
 
 fn main() {
     pretty_env_logger::init();
-    alphazero_rs::arena::play_game(&[&ask_for_action, &ask_for_action], &None, true);
+    let mut coach = Coach::setup(
+        "./checkpoint", // checkpoint_directory
+        1000000,        // mcts_reserve_size
+        0.6,            // update_threshold
+        15,             // temp_threshold
+        20,             // max_history_length
+        200000,         // max_queue_length
+        1,              // inference_batch_size
+        1,              // num_episode_threads
+        40,             // num_arena_games
+        1,              // num_iters
+        1,              // num_eps
+        25,             // num_sims
+        1,              // num_sim_threads,
+        1000,           // max_depth
+        1,              // cpuct
+    );
+    coach.learn::<ConnectFourGame, DumbConnectFourNnet, String>(
+        "./checkpoint".to_string(),
+        false,
+        true,
+        &mut SmallRng::from_entropy(),
+    );
+
+    //alphazero_rs::arena::play_game(&[&ask_for_action, &ask_for_action], &None, true);
 }
